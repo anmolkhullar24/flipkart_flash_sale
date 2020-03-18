cf326cc933630c2cf986a78498cf0bd00f7492d9
raghavkhullar <raghavkhullar@yahoo.com>
2020-03-18 14:09:58 +0530

    Update flash.py

--- flash.py
+++ flash.py
@@ -1,4 +1,41 @@
+from selenium import webdriver
 from requests import get
-url = "https://www.flipkart.com/poco-x2-atlantis-blue-64-gb/p/itmbe7b58e0378b8?pid=MOBFZGJ6ZGMD7GEZ&lid=LSTMOBFZGJ6ZGMD7GEZAOQLPQ&marketplace=FLIPKART&srno=s_1_1&otracker=AS_QueryStore_OrganicAutoSuggest_0_6_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_0_6_na_na_na&fm=SEARCH&iid=7c67262f-1cc3-4b01-97ea-bf01225e1c8a.MOBFZGJ6ZGMD7GEZ.SEARCH&ppt=sp&ppn=sp&ssid=8fnjep2t740000001584457474420&qH=9b046b567ecc0251"
-response = get(url)
-print(response.text[:500])
+import time
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+driver = webdriver.Chrome("F:/WORK/chromedriver_win32/chromedriver_win32/chromedriver")
+url = "https://www.flipkart.com/poco-f1-steel-blue-256-gb/p/itmf8qpcumze7msy?pid=MOBF85V7SSRXGFRZ&lid=LSTMOBF85V7SSRXGFRZYF68TE&marketplace=FLIPKART&srno=s_1_10&otracker=AS_QueryStore_OrganicAutoSuggest_0_6_na_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_0_6_na_na_na&fm=organic&iid=9166a6de-1d95-4de9-823a-2065b0dd214b.MOBF85V7SSRXGFRZ.SEARCH&ssid=rc5q8mhg9c0000001584519031461&qH=9b046b567ecc0251"
+driver.get(url)
+driver.implicitly_wait(10)
+username = "7837223239"
+password = "khullar123"
+def buy_now():
+    while True:
+        # response = get(url)
+        if "BUY NOW" in driver.page_source:
+            driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/div[1]/div[1]/div[2]/div[1]/ul[1]/li[2]/form[1]/button[1]").click()
+            break
+        else:
+            driver.refresh()
+            print("refresh")
+def login():
+    if "Enter Email/Mobile number" in driver.page_source:
+        driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/input[1]").send_keys(username)
+        driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[2]/button[1]").click()
+    else :
+        driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[1]/input[1]").send_keys(username)
+        driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[2]/button[1]").click()
+        driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[2]/input[1]").send_keys(password)
+        driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/form[1]/div[3]/button[1]").click()
+        
+def delivery():
+    driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/label[1]/div[2]/div[1]/div[1]/button[1]").click()
+
+def order_confim():
+    driver.find_element_by_xpath("/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[2]/div[2]/span[2]/button[1]").click()
+
+
+buy_now()
+login()
+delivery()
+order_confim()
